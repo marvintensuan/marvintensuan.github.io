06172f6a7c7ac52a51accd54befe7cf9cf241a55
Marvin <mdtensuan_dev@protonmail.com>
2021-03-06 17:23:49 +0800

    Delete removed elements.

--- js/main.js
+++ js/main.js
@@ -1,67 +1,5 @@
-//Navigation
+//Navigation Bar
 $(".nav-link").eq(0).addClass("active")
-//For Dark Mode
-function goDarkMode(){
-    if (document.getElementById("light-dark").innerText == "Dark"){
-        document.documentElement.setAttribute('data-theme', 'dark')
-        //button
-        $("#light-dark").addClass("btn-light").removeClass("btn-dark")
-        document.getElementById("light-dark").innerText = "Light"
-        $("body").addClass("text-white")
-        $(".jumbotron").addClass("bg-dark")
-        $("hr").addClass("border-white")
-        $("#img-switch1").attr('src', 'img/hourglass-primary.png')
-        $("#img-switch2").attr('src', 'img/cmd-line-primary.png')
-        $("#img-pandas").attr('src', 'img/pandas-primary.png')
-        $("#img-web").attr('src', 'img/web-primary.png')
-        $("#img-cloud").attr('src', 'img/cloud-primary.png')
-        $("#img-db").attr('src', 'img/db-primary.png')
-        $(".img-notebook").attr('src', 'img/notebook-primary.png')
-        $("#img-money").attr('src', 'img/money-primary.png')
-        $("#img-robot").attr('src', 'img/robot-primary.png')
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light')
-        //button
-        $("#light-dark").addClass("btn-dark").removeClass("btn-light")
-        document.getElementById("light-dark").innerText = "Dark";
-        $("body").removeClass("text-white")
-        $(".jumbotron").removeClass("bg-dark")
-        $("hr").removeClass("border-white")
-        $("#img-switch1").attr('src', 'img/hourglass.png')
-        $("#img-switch2").attr('src', 'img/cmd-line.png')
-        $("#img-pandas").attr('src', 'img/pandas.png')
-        $("#img-web").attr('src', 'img/web.png')
-        $("#img-cloud").attr('src', 'img/cloud.png')
-        $("#img-db").attr('src', 'img/db.png')
-        $(".img-notebook").attr('src', 'img/notebook.png')
-        $("#img-money").attr('src', 'img/money.png')
-        $("#img-robot").attr('src', 'img/robot.png')
-    }
-}
-let toggleButton = document.getElementById("light-dark")
-toggleButton.addEventListener('click', goDarkMode)
-
-//format
-$(".card").hover(
-    function() {
-        $(this).addClass('shadow-lg'); 
-    },
-    function() {
-        $(this).removeClass('shadow-lg');
-});
-$(".card-text").addClass("text-justify")
-
-$("#repo-title").hover(
-    function() {
-        $(this).text("github.com/marvintensuan"); 
-    },
-    function() {
-        $(this).text("My GitHub Repository"); 
-});
-
-let today = new Date()
-$("#work-xp").text(today.getFullYear() - 2018)
 
 $(function(){
     $(window).bind("resize",function(){
@@ -73,3 +11,62 @@ $(function(){
         }
     })
 })
+
+//For Dark Mode
+function goDarkMode(){
+    if (document.getElementById("light-dark").innerText == "Dark"){
+        document.documentElement.setAttribute('data-theme', 'dark')
+        //button
+        $("#light-dark").addClass("btn-light").removeClass("btn-dark")
+        document.getElementById("light-dark").innerText = "Light"
+        $("body").addClass("text-white")
+        $(".jumbotron").addClass("bg-dark")
+        $("hr").addClass("border-white")
+        $("#img-web").attr('src', 'img/web-primary.png')
+        $("#img-cloud").attr('src', 'img/cloud-primary.png')
+        $("#img-db").attr('src', 'img/db-primary.png')
+        $("#img-money").attr('src', 'img/money-primary.png')
+        $("#img-robot").attr('src', 'img/robot-primary.png')
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light')
+        //button
+        $("#light-dark").addClass("btn-dark").removeClass("btn-light")
+        document.getElementById("light-dark").innerText = "Dark";
+        $("body").removeClass("text-white")
+        $(".jumbotron").removeClass("bg-dark")
+        $("hr").removeClass("border-white")
+        $("#img-web").attr('src', 'img/web.png')
+        $("#img-cloud").attr('src', 'img/cloud.png')
+        $("#img-db").attr('src', 'img/db.png')
+        $("#img-money").attr('src', 'img/money.png')
+        $("#img-robot").attr('src', 'img/robot.png')
+    }
+}
+let toggleButton = document.getElementById("light-dark")
+toggleButton.addEventListener('click', goDarkMode)
+
+/** 
+Format
+Add shadows to cards on mouse hover. **/
+$(".card").hover(
+    function() {
+        $(this).addClass('shadow-lg'); 
+    },
+    function() {
+        $(this).removeClass('shadow-lg');
+});
+$(".card-text").addClass("text-justify")
+
+// Display link to My Repository
+$("#repo-title").hover(
+    function() {
+        $(this).text("github.com/marvintensuan"); 
+    },
+    function() {
+        $(this).text("My GitHub Repository"); 
+});
+
+// Calculate work experiences
+let today = new Date()
+$("#work-xp").text(today.getFullYear() - 2018)
